75e193b9d0dd0d4ef17f01752a208847d0b42608
Sergey Mikolenko <mmmaybach@yandex.ru>
2023-12-11 00:19:19 +0500

    Готовый проект 2

--- scripts/cards.js
+++ scripts/cards.js
@@ -24,25 +24,3 @@ const initialCards = [
     link: "../images/Labytnangi.webp",
   },
 ];
-
-const deleteCard = (evt) => evt.target.closest(".places__item").remove();
-
-const cardsTemplate = document.querySelector("#card-template").content;
-const cardsList = document.querySelector(".places__list");
-
-const createCard = (link, name) => {
-  const card = cardsTemplate.querySelector(".places__item").cloneNode(true);
-  const deleteButton = card.querySelector(".card__delete-button");
-
-  card.querySelector(".card__image").src = link;
-  card.querySelector(".card__image").alt = name;
-  card.querySelector(".card__title").textContent = name;
-
-  deleteButton.addEventListener("click", deleteCard);
-
-  return card;
-};
-
-initialCards.forEach((card) => cardsList.append(createCard(card.link, card.name)));
-
-
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
+const deleteCard = (evt) => evt.target.closest(".places__item").remove();
 
+const cardsTemplate = document.querySelector("#card-template").content;
+const cardsList = document.querySelector(".places__list");
+
+const createCard = (cardData, deleteHandler) => {
+  const { link, name } = cardData;
+
+  const card = cardsTemplate.querySelector(".places__item").cloneNode(true);
+  const deleteButton = card.querySelector(".card__delete-button");
+
+  card.querySelector(".card__image").src = link;
+  card.querySelector(".card__image").alt = name;
+  card.querySelector(".card__title").textContent = name;
+
+  deleteButton.addEventListener("click", deleteHandler);
+
+  return card;
+};
+
+initialCards.forEach((card) => cardsList.append(createCard(card, deleteCard)));
