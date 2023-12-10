36f1acf344ded504398d4b0cb1c1802b398e8847
Sergey Mikolenko <mmmaybach@yandex.ru>
2023-12-10 19:16:31 +0500

    Готовый Проект

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,48 @@
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Салехард",
+    link: "../images/Salekhard.webp",
+  },
+  {
+    name: "Надым",
+    link: "../images/Nadym.webp",
+  },
+  {
+    name: "Новый Уренгой",
+    link: "../images/NovyYrengoy.webp",
+  },
+  {
+    name: "Муравленко",
+    link: "../images/Myravlenko.webp",
+  },
+  {
+    name: "Губкинский",
+    link: "../images/Gubkinsky.webp",
+  },
+  {
+    name: "Лабытнанги",
+    link: "../images/Labytnangi.webp",
+  },
 ];
+
+const deleteCard = (evt) => evt.target.closest(".places__item").remove();
+
+const cardsTemplate = document.querySelector("#card-template").content;
+const cardsList = document.querySelector(".places__list");
+
+const createCard = (link, name) => {
+  const card = cardsTemplate.querySelector(".places__item").cloneNode(true);
+  const deleteButton = card.querySelector(".card__delete-button");
+
+  card.querySelector(".card__image").src = link;
+  card.querySelector(".card__image").alt = name;
+  card.querySelector(".card__title").textContent = name;
+
+  deleteButton.addEventListener("click", deleteCard);
+
+  return card;
+};
+
+initialCards.forEach((card) => cardsList.append(createCard(card.link, card.name)));
+
+
